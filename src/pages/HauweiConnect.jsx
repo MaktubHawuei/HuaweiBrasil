85c7491a46ba2c66dd161df5fe7cc12fd2fb2b4c
realclownblack <danilowilliam3255@gmail.com>
2022-10-19 09:24:24 -0300

    19/10/2022 - 09:24

---
+++ src/pages/HauweiConnect.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../assets/css/HauweiConnect.css'
+class HauweiConnect extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <>
+            </>
+        );
+    }
+}
+ 
+export default HauweiConnect;
